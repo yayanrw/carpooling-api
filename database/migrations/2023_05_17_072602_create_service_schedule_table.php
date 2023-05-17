b511a27242874a5c4cf55360c04e1b3d935dd5e5
yayanrw <yayanraw@gmail.com>
2023-05-17 14:30:41 +0700

    feat: create 2023_05_17_072602_create_service_schedule_table.php

---
+++ database/migrations/2023_05_17_072602_create_service_schedule_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('t_service_schedule', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('vehicle_id');
+            $table->date('estimation_date');
+            $table->date('actual_date')->nullable();
+            $table->boolean('is_serviced')->nullable();
+            $table->timestamps();
+            $table->unsignedBigInteger('created_by');
+        });
+
+        Schema::table('t_service_schedule', function ($table) {
+            $table->foreign('vehicle_id')->references('id')->on('m_vehicle')->onDelete('cascade');
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('t_service_schedule');
+    }
+};
