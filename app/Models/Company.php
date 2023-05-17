bbb53fc2521249e4076409e7047e3a4e43768b77
yayanrw <yayanraw@gmail.com>
2023-05-17 16:36:51 +0700

    feat: create Company.php

---
+++ app/Models/Company.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Company extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'm_company';
+
+    protected $fillable = [
+        'company_name', 'created_by', 'updated_by', 'deleted_by',
+    ];
+
+    public function createdBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function updatedBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function deletedBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
