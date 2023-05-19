abf3ced0f1a9cc4f9076b0ad40318090ad0ad87d
yayanrw <yayanraw@gmail.com>
2023-05-19 14:43:33 +0700

    feat: create FuelConsumption.php

---
+++ app/Models/FuelConsumption.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class FuelConsumption extends Model
+{
+    use HasFactory;
+
+    protected $table = 't_fuel_consumtion';
+
+    protected $fillable = [
+        'vehicle_id', 'date', 'litres', 'created_by'
+    ];
+
+    public function vehicle(): BelongsTo
+    {
+        return $this->belongsTo(Vehicle::class);
+    }
+
+    public function createdBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
