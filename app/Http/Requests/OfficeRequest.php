8238607d83975d60c45e124616b7cc1e4791f5be
yayanrw <yayanraw@gmail.com>
2023-05-19 13:52:40 +0700

    feat: create OfficeRequest.php

---
+++ app/Http/Requests/OfficeRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class OfficeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'company_id' => ['required', Rule::exists('m_company', 'id')],
+            'office_name' => ['required', 'string'],
+            'region' => ['string'],
+            'type' => ['required', 'string', 'in:headoffice,supportoffice,jobsite'],
+        ];
+    }
+}
