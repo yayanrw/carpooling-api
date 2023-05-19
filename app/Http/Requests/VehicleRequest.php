323b9fe5c0e45dd508bf06ca9cbc2f3ed7ee6dbb
yayanrw <yayanraw@gmail.com>
2023-05-19 14:33:38 +0700

    feat: create VehicleRequest.php

---
+++ app/Http/Requests/VehicleRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class VehicleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'company_id' => ['required', Rule::exists('m_company', 'id')],
+            'office_id' => ['required', Rule::exists('m_office', 'id')],
+            'license_plate' => ['required', 'string', 'unique:m_vehicle,license_plate'],
+            'hull_number' => ['required', 'string', 'unique:m_vehicle,hull_number'],
+            'type' => ['required', 'in:manhauling,goodshauling']
+        ];
+    }
+}
