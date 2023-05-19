015f7218a88acf1499354bd21fc8a082c97e955f
yayanrw <yayanraw@gmail.com>
2023-05-19 16:58:58 +0700

    feat: create ServiceSchedule.php

---
+++ app/Models/ServiceSchedule.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ServiceSchedule extends Model
+{
+    use HasFactory;
+
+    protected $table = 't_service_schedule';
+
+    protected $fillable = [
+        'vehicle_id', 'estimation_date', 'actual_date', 'is_serviced', 'created_by', 'updated_by',
+    ];
+
+    public function vehicle(): BelongsTo
+    {
+        return $this->belongsTo(Vehicle::class);
+    }
+
+    public function createdBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function updatedBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
