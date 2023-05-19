8be214401b84cbb9740a6d2976fa655f102f10ad
yayanrw <yayanraw@gmail.com>
2023-05-19 13:59:40 +0700

    feat: create Office.php

---
+++ app/Models/Office.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Office extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'm_office';
+
+    protected $fillable = [
+        'company_id', 'office_name', 'region', 'type', 'created_by', 'updated_by', 'deleted_by',
+    ];
+
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function createdBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function updatedBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function deletedBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
