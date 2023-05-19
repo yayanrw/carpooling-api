ed604adde983710953bab08b023e2560685b9429
yayanrw <yayanraw@gmail.com>
2023-05-19 16:17:12 +0700

    update FuelConsumptionController.php

--- app/Http/Controllers/FuelConsumptionController.php
+++ app/Http/Controllers/FuelConsumptionController.php
@@ -2,84 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\FuelConsumptionRequest;
 use App\Models\FuelConsumption;
+use App\MyApp;
+use App\Traits\HttpResponses;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FuelConsumptionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    use HttpResponses;
+
     public function index()
     {
-        //
+        try {
+            $fuelConsumption = FuelConsumption::withoutTrashed()->get();
+            return $this->success($fuelConsumption);
+        } catch (Exception $e) {
+            return $this->error(null, $e->getMessage(), MyApp::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function store(FuelConsumptionRequest $fuelConsumptionRequest)
     {
-        //
+        try {
+            $fuelConsumptionRequest->validated($fuelConsumptionRequest->all());
+
+            $fuelConsumption = FuelConsumption::create([
+                'vehicle_id' => $fuelConsumptionRequest->vehicle_id,
+                'date' => $fuelConsumptionRequest->date,
+                'litres' => $fuelConsumptionRequest->litres,
+                'created_by' => Auth::user()->id,
+            ]);
+
+            return $this->success($fuelConsumption, MyApp::INSERTED_SUCCESSFULLY);
+        } catch (Exception $e) {
+            return $this->error(null, $e->getMessage(), MyApp::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function show(int $id)
     {
-        //
-    }
+        try {
+            $fuelConsumption = FuelConsumption::find($id);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\FuelConsumption  $fuelConsumption
-     * @return \Illuminate\Http\Response
-     */
-    public function show(FuelConsumption $fuelConsumption)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\FuelConsumption  $fuelConsumption
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(FuelConsumption $fuelConsumption)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\FuelConsumption  $fuelConsumption
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, FuelConsumption $fuelConsumption)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\FuelConsumption  $fuelConsumption
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(FuelConsumption $fuelConsumption)
-    {
-        //
+            if (!empty($fuelConsumption)) {
+                return $this->success($fuelConsumption);
+            } else {
+                return $this->error(null, MyApp::DATA_NOT_FOUND, MyApp::HTTP_NO_CONTENT);
+            }
+        } catch (Exception $e) {
+            return $this->error(null, $e->getMessage(), MyApp::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
