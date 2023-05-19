6a42ec7bba74a485f3749722f056f64f79875a7d
yayanrw <yayanraw@gmail.com>
2023-05-19 13:19:36 +0700

    update app.php add aliasses

--- config/app.php
+++ config/app.php
@@ -210,6 +210,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'MyApp' => App\MyApp::class,
     ])->toArray(),
 
 ];
