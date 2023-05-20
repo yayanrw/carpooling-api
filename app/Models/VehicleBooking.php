450f4ee3c8fd24224c559eabd3f0dd82cbf4cc81
yayanrw <yayanraw@gmail.com>
2023-05-20 15:18:21 +0700

    update VehicleBooking.php

--- app/Models/VehicleBooking.php
+++ app/Models/VehicleBooking.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class VehicleBooking extends Model
 {
@@ -48,5 +49,10 @@ class VehicleBooking extends Model
     public function updatedBy(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function approvalRequests(): HasMany
+    {
+        return $this->hasMany(ApprovalRequest::class);
     }
 }
