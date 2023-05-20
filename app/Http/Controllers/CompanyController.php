e8f9c507e7c1b6666583bd0e5e1e566bdcca2e25
yayanrw <yayanraw@gmail.com>
2023-05-20 21:24:15 +0700

    update CompanyController.php

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -45,55 +45,37 @@ class CompanyController extends Controller
         }
     }
 
-    public function show(int $id)
+    public function show(Company $company)
     {
         try {
-            $company = Company::find($id);
-
-            if (!empty($company)) {
-                return $this->success($company);
-            } else {
-                return $this->error(null, MyApp::DATA_NOT_FOUND, MyApp::HTTP_NO_CONTENT);
-            }
+            return $this->success($company);
         } catch (Exception $e) {
             return $this->error(null, $e->getMessage(), MyApp::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
-    public function update(CompanyRequest $companyRequest, int $id)
+    public function update(CompanyRequest $companyRequest, Company $company)
     {
         try {
-            $company = Company::find($id);
+            $company->company_name = $companyRequest->company_name;
+            $company->updated_by = Auth::user()->id;
 
-            if (!empty($company)) {
-                $company->company_name = $companyRequest->company_name;
-                $company->updated_by = Auth::user()->id;
+            $company->save();
 
-                $company->save();
-
-                return $this->success($company, MyApp::UPDATED_SUCCESSFULLY);
-            } else {
-                return $this->error(null, MyApp::DATA_NOT_FOUND, MyApp::HTTP_NO_CONTENT);
-            }
+            return $this->success($company, MyApp::UPDATED_SUCCESSFULLY);
         } catch (Exception $e) {
             return $this->error(null, $e->getMessage(), MyApp::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
-    public function destroy(int $id)
+    public function destroy(Company $company)
     {
         try {
-            $company = Company::find($id);
+            $company->deleted_by = Auth::user()->id;
+            $company->save();
+            $company->delete();
 
-            if (!empty($company)) {
-                $company->deleted_by = Auth::user()->id;
-                $company->save();
-                $company->delete();
-
-                return $this->success(null, MyApp::DELETED_SUCCESSFULLY);
-            } else {
-                return $this->error(null, MyApp::DATA_NOT_FOUND, MyApp::HTTP_NO_CONTENT);
-            }
+            return $this->success(null, MyApp::DELETED_SUCCESSFULLY);
         } catch (Exception $e) {
             return $this->error(null, $e->getMessage(), MyApp::HTTP_INTERNAL_SERVER_ERROR);
         }
