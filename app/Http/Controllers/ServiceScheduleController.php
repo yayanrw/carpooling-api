0ae19d20247697353d54f16c73ddb5aaac96681c
yayanrw <yayanraw@gmail.com>
2023-05-20 22:23:32 +0700

    update ServiceScheduleController.php

--- app/Http/Controllers/ServiceScheduleController.php
+++ app/Http/Controllers/ServiceScheduleController.php
@@ -46,38 +46,26 @@ class ServiceScheduleController extends Controller
         }
     }
 
-    public function show(int $id)
+    public function show(ServiceSchedule $serviceSchedule)
     {
         try {
-            $serviceSchedule = ServiceSchedule::find($id);
-
-            if (!empty($serviceSchedule)) {
-                return $this->success($serviceSchedule);
-            } else {
-                return $this->error(null, MyApp::DATA_NOT_FOUND, MyApp::HTTP_NO_CONTENT);
-            }
+            return $this->success($serviceSchedule);
         } catch (Exception $e) {
             return $this->error(null, $e->getMessage(), MyApp::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
-    public function update(UpdateServiceScheduleRequest $updateServiceScheduleRequest, int $id)
+    public function update(UpdateServiceScheduleRequest $updateServiceScheduleRequest, ServiceSchedule $serviceSchedule)
     {
         try {
-            $serviceSchedule = ServiceSchedule::find($id);
+            $serviceSchedule->vehicle_id = $updateServiceScheduleRequest->vehicle_id;
+            $serviceSchedule->actual_date = $updateServiceScheduleRequest->actual_date;
+            $serviceSchedule->is_serviced = $updateServiceScheduleRequest->is_serviced;
+            $serviceSchedule->updated_by = Auth::user()->id;
 
-            if (!empty($serviceSchedule)) {
-                $serviceSchedule->vehicle_id = $updateServiceScheduleRequest->vehicle_id;
-                $serviceSchedule->actual_date = $updateServiceScheduleRequest->actual_date;
-                $serviceSchedule->is_serviced = $updateServiceScheduleRequest->is_serviced;
-                $serviceSchedule->updated_by = Auth::user()->id;
+            $serviceSchedule->save();
 
-                $serviceSchedule->save();
-
-                return $this->success($serviceSchedule, MyApp::UPDATED_SUCCESSFULLY);
-            } else {
-                return $this->error(null, MyApp::DATA_NOT_FOUND, MyApp::HTTP_NO_CONTENT);
-            }
+            return $this->success($serviceSchedule, MyApp::UPDATED_SUCCESSFULLY);
         } catch (Exception $e) {
             return $this->error(null, $e->getMessage(), MyApp::HTTP_INTERNAL_SERVER_ERROR);
         }
