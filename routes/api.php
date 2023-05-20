656f8c33c8849ed55f279588e65c08f4fe146332
yayanrw <yayanraw@gmail.com>
2023-05-20 23:16:39 +0700

    update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Http\Controllers\ApprovalRequestController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\FuelConsumptionController;
 use App\Http\Controllers\OfficeController;
-use App\Models\Vehicle;
+use App\Http\Controllers\ServiceScheduleController;
+use App\Http\Controllers\VehicleBookingController;
+use App\Http\Controllers\VehicleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -32,49 +36,40 @@ Route::controller(AuthController::class)->group(function () {
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
-    Route::middleware(['ability:super_admin,admin'])->group(function () {
-        Route::post('/company', [CompanyController::class, 'store']);
-        Route::put('/company/{company}', [CompanyController::class, 'update']);
-        Route::delete('/company/{company}', [CompanyController::class, 'destroy']);
-
-        Route::post('/office', [OfficeController::class, 'store']);
-        Route::put('/office/{office}', [OfficeController::class, 'update']);
-        Route::delete('/office/{office}', [OfficeController::class, 'destroy']);
-
-        Route::post('/vehicle', [VehicleController::class, 'store']);
-        Route::put('/vehicle/{vehicle}', [VehicleController::class, 'update']);
-        Route::delete('/vehicle/{vehicle}', [VehicleController::class, 'destroy']);
-
-        Route::post('/approval-requests', [ApprovalRequestController::class, 'store']);
-        Route::put('/approval-requests/{approvalRequest}', [ApprovalRequestController::class, 'update']);
-
-        Route::post('/fuel-consumptions', [FuelConsumptionController::class, 'store']);
-
-        Route::post('/service-schedules', [ServiceScheduleController::class, 'store']);
-        Route::put('/service-schedules/{serviceSchedule}', [ServiceScheduleController::class, 'update']);
-
-        Route::post('/vehicle-bookings', [VehicleBookingController::class, 'store']);
-        Route::put('/vehicle-bookings/{vehicleBooking}', [VehicleBookingController::class, 'update']);
-    });
+    Route::get('/approval-requests', [ApprovalRequestController::class, 'index']);
+    Route::get('/approval-requests/{approvalRequest}', [ApprovalRequestController::class, 'show']);
+    Route::post('/approval-requests', [ApprovalRequestController::class, 'store']);
+    Route::put('/approval-requests/{approvalRequest}', [ApprovalRequestController::class, 'update']);
 
     Route::get('/company', [CompanyController::class, 'index']);
     Route::get('/company/{company}', [CompanyController::class, 'show']);
-
-    Route::get('/office', [OfficeController::class, 'index']);
-    Route::get('/office/{office}', [OfficeController::class, 'show']);
-
-    Route::get('/vehicle', [VehicleController::class, 'index']);
-    Route::get('/vehicle/{vehicle}', [VehicleController::class, 'show']);
-
-    Route::get('/approval-requests', [ApprovalRequestController::class, 'index']);
-    Route::get('/approval-requests/{approvalRequest}', [ApprovalRequestController::class, 'show']);
+    Route::post('/company', [CompanyController::class, 'store']);
+    Route::put('/company/{company}', [CompanyController::class, 'update']);
+    Route::delete('/company/{company}', [CompanyController::class, 'destroy']);
 
     Route::get('/fuel-consumptions', [FuelConsumptionController::class, 'index']);
     Route::get('/fuel-consumptions/{fuelConsumption}', [FuelConsumptionController::class, 'show']);
+    Route::post('/fuel-consumptions', [FuelConsumptionController::class, 'store']);
+
+    Route::get('/office', [OfficeController::class, 'index']);
+    Route::get('/office/{office}', [OfficeController::class, 'show']);
+    Route::post('/office', [OfficeController::class, 'store']);
+    Route::put('/office/{office}', [OfficeController::class, 'update']);
+    Route::delete('/office/{office}', [OfficeController::class, 'destroy']);
 
     Route::get('/service-schedules', [ServiceScheduleController::class, 'index']);
     Route::get('/service-schedules/{serviceSchedule}', [ServiceScheduleController::class, 'show']);
+    Route::post('/service-schedules', [ServiceScheduleController::class, 'store']);
+    Route::put('/service-schedules/{serviceSchedule}', [ServiceScheduleController::class, 'update']);
 
     Route::get('/vehicle-bookings', [VehicleBookingController::class, 'index']);
     Route::get('/vehicle-bookings/{vehicleBooking}', [VehicleBookingController::class, 'show']);
+    Route::post('/vehicle-bookings', [VehicleBookingController::class, 'store']);
+    Route::put('/vehicle-bookings/{vehicleBooking}', [VehicleBookingController::class, 'update']);
+
+    Route::get('/vehicle', [VehicleController::class, 'index']);
+    Route::get('/vehicle/{vehicle}', [VehicleController::class, 'show']);
+    Route::post('/vehicle', [VehicleController::class, 'store']);
+    Route::put('/vehicle/{vehicle}', [VehicleController::class, 'update']);
+    Route::delete('/vehicle/{vehicle}', [VehicleController::class, 'destroy']);
 });
